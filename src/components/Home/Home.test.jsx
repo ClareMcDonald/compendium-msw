c3a50b044b491360ad3f08c3192376d5ec28db51
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-04-29 11:02:12 -0700

    work on component test

--- src/components/Home/Home.test.jsx
+++ src/components/Home/Home.test.jsx
@@ -1,5 +1,33 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import { rest } from 'msw';
-import { setUpServer } from 'msw/node';
+import { setupServer } from 'msw/node';
 import Home from './Home.jsx';
 
+const movie = [ {
+    image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg',
+    title: 'Grave of the Fireflies',
+    originalTitle: '火垂るの墓',
+    director: 'Isao Takahata',
+    releaseDate: '1988',
+    description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.'
+}]
+
+const server = setupServer(
+    rest.get('https://ghibliapi.herokuapp.com/films', (req, res, ctx) => res(ctx.json(movie)))
+);
+
+describe('Home', () => {
+    beforeAll(() => server.listen());
+    afterAll(() => server.close());
+
+    test('Should render a movie', async () => {
+        render(<Home />)
+        return waitFor(() => {
+            const name = screen.getByText('Grave of the Fireflies', { exact: false })
+            const releaseDate = screen.getByText('1988', { exact: false })
+    
+            expect(name).toBeInTheDocument()
+            expect(releaseDate).toBeInTheDocument();
+        });
+    });
+});
