a537684703e381e323a533b9981313441804065e
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-04-29 11:10:57 -0700

    component test passes

--- src/components/Home/Home.test.jsx
+++ src/components/Home/Home.test.jsx
@@ -6,9 +6,9 @@ import Home from './Home.jsx';
 const movie = [ {
     image: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/qG3RYlIVpTYclR9TYIsy8p7m7AT.jpg',
     title: 'Grave of the Fireflies',
-    originalTitle: '火垂るの墓',
+    original_title: '火垂るの墓',
     director: 'Isao Takahata',
-    releaseDate: '1988',
+    release_date: '1988',
     description: 'In the latter part of World War II, a boy and his sister, orphaned when their mother is killed in the firebombing of Tokyo, are left to survive on their own in what remains of civilian life in Japan. The plot follows this boy and his sister as they do their best to survive in the Japanese countryside, battling hunger, prejudice, and pride in their own quiet, personal battle.'
 }]
 
